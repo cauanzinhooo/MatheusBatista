08f6afb1db1cac523a5a43effc4037270ab502e1
Cauanzinhooo <cauan21gomes@hotmail.com>
2024-03-09 03:29:42 -0300

    ftd

--- app/components/Sections/Services/index.tsx
+++ app/components/Sections/Services/index.tsx
@@ -1,13 +1,40 @@
+'use client'
+import Carousel from 'nuka-carousel'
+import { Servicesdata } from '@/app/utils/variables'
+
 const Services = () => {
+  const renderBottomCenterControls = ({ currentSlide }: any) => (
+    <div className="absolute right-0 top-0 flex w-[400px] flex-col justify-around bg-red-400">
+      <button style={{ color: currentSlide === 0 ? 'red' : 'black' }}>1</button>
+      <button style={{ color: currentSlide === 1 ? 'red' : 'black' }}>2</button>
+      <button style={{ color: currentSlide === 2 ? 'red' : 'black' }}>3</button>
+    </div>
+  )
+
   return (
     <section className="mt-[86px]">
       <h2 className="ext- bg-gradient-to-r from-[#984FFF] to-[#2C0F55] bg-clip-text text-center font-sans text-[40px] font-extrabold text-transparent">
         Quais serviços você encontrará aqui?
       </h2>
-      <div className="container mx-auto">
-        <div></div>
+      <div className="mb-100 container mx-auto  py-20">
+        <div className="mx-5">
+          <Carousel
+            renderBottomCenterControls={renderBottomCenterControls}
+            className="shadow-[50px 0px 50px 20px] relative rounded-[40px] bg-[#1A1A1A] shadow-purple-700"
+          >
+            {Servicesdata.map((i) => (
+              <div className="mx-5 flex flex-col " key={i.id}>
+                <h2 className="max-w-96 font-sans text-4xl font-semibold">
+                  {i.title}
+                </h2>
+                <p>{i.description}</p>
+              </div>
+            ))}
+          </Carousel>
+        </div>
       </div>
     </section>
   )
 }
+
 export default Services
--- app/utils/variables.tsx
+++ app/utils/variables.tsx
@@ -30,3 +30,36 @@ export const Socialsdata = [
       'A maior captação de leads por vídeo com certeza vem do YouTube,o que pode contribuir muito ter um canal e frequência nas postagens',
   },
 ]
+
+export const Servicesdata = [
+  {
+    id: 1,
+    title: 'Análise de Dados e Pesquisa de Mercado:',
+    description:
+      'Coleta, análise e interpretação de dados relevantes sobre o tráfego na web, incluindo tendências do mercado, comportamento do consumidor e concorrência.',
+  },
+  {
+    id: 2,
+    title: 'Planejamento Estratégico de Campanhas: ',
+    description:
+      'Desenvolvimento de estratégias de marketing digital personalizadas com base nos dados coletados, considerando objetivos de negócios, orçamento disponível e público-alvo.',
+  },
+  {
+    id: 3,
+    title: 'Implementação e Otimização de Campanhas:',
+    description:
+      'Configuração e gestão de campanhas de publicidade online em diversas plataformas, como Google Ads, Facebook Ads, entre outras. Constante otimização das campanhas para maximizar o retorno sobre o investimento (ROI), incluindo ajustes de lances, segmentação, criativos e palavras-chave.',
+  },
+  {
+    id: 4,
+    title: 'Gestão de Orçamento e Lances:',
+    description:
+      'Alocação eficiente do orçamento de marketing para diferentes canais e campanhas, garantindo uma distribuição equilibrada dos recursos e maximizando o desempenho das campanhas. Gerenciamento de lances para garantir que os anúncios sejam exibidos de forma competitiva e eficaz.',
+  },
+  {
+    id: 5,
+    title: 'Monitoramento e Análise de Desempenho:',
+    description:
+      'Acompanhamento contínuo do desempenho das campanhas, monitorando métricas-chave como cliques, impressões, conversões e custo por aquisição (CPA). Análise dos dados para identificar padrões, tendências e oportunidades de melhoria, e ajuste das estratégias conforme necessário.',
+  },
+]
